a876c803e95f9498d7dc5aaec9a35c886fdbcd8b
milidream <bchekir07@gmail.com>
2022-02-23 13:13:41 +0100

    All

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-let month = document.querySelector("#month");
+let mois = document.querySelector("#mois");
 let previous = document.querySelector("#previous");
 let next = document.querySelector("#next");
-let jour = document.querySelector(".day");
+let jours = document.querySelector("#days");
 
-let add = querySelector("#add");
-let task = querySelector("#task");
-let tasks = querySelector("tasks");
+const allMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+const now = new Date();
+let name = allMonth[now.getMonth()];
+
+let getDaysInMonth = function(month,year) {
+  return new Date(year, month, 0).getDate();
+};
+console.log(getDaysInMonth(1, 2012));
+
+function generateDays(){
+  for(let i=1; i<getDaysInMonth(6, 2022)+1; i++){
+    let newBtn = document.createElement("button");
+    newBtn.setAttribute("class", "day")
+    let numero = document.createTextNode(i);
+    newBtn.appendChild(numero)
+    jours.appendChild(newBtn);
+  }
+}
+generateDays();
+
+function changeMonth(){
+
+}
