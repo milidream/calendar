5862e7ef1e27824fdd87410553dac47760d6aa9d
milidream <bchekir07@gmail.com>
2022-02-23 15:58:56 +0100

    All

--- src/index.js
+++ src/index.js
@@ -2,27 +2,68 @@ let mois = document.querySelector("#mois");
 let previous = document.querySelector("#previous");
 let next = document.querySelector("#next");
 let jours = document.querySelector("#days");
+let annee = document.querySelector("#annee");
 
-const allMonth = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const now = new Date();
-let name = allMonth[now.getMonth()];
 
-let getDaysInMonth = function(month,year) {
+now.setMonth(now.getMonth());
+  let ratay = now.toString();
+  let words = ratay.split(' ');
+  mois.innerHTML = words[1];
+
+now.getMonth();
+function getDaysInMonth(month,year) {
   return new Date(year, month, 0).getDate();
 };
-console.log(getDaysInMonth(1, 2012));
 
 function generateDays(){
-  for(let i=1; i<getDaysInMonth(6, 2022)+1; i++){
+  for(let i=1; i<getDaysInMonth(now.getMonth()+1, now.getFullYear())+1; i++){
     let newBtn = document.createElement("button");
-    newBtn.setAttribute("class", "day")
+    newBtn.setAttribute("class", "day");
     let numero = document.createTextNode(i);
-    newBtn.appendChild(numero)
+    newBtn.appendChild(numero);
     jours.appendChild(newBtn);
-  }
-}
+  };
+};
+
 generateDays();
 
-function changeMonth(){
+function suivant(){
+  now.setMonth(now.getMonth()+1);
+  let ratay = now.toString();
+  let words = ratay.split(' ');
+  mois.innerHTML = words[1];
+  annee.innerHTML = now.getFullYear();
+  console.log(now);
+  while(jours.hasChildNodes()){  
+    jours.removeChild(jours.firstChild);
+  };
+  for(let i=1; i<getDaysInMonth(now.getMonth()+1, now.getFullYear())+1; i++){
+    let newBtn = document.createElement("button");
+    newBtn.setAttribute("class", "day");
+    let numero = document.createTextNode(i);
+    newBtn.appendChild(numero);
+    jours.appendChild(newBtn);
+  };
+};
+next.addEventListener("click", suivant);
 
-}
+function precedent(){
+  now.setMonth(now.getMonth()-1);
+  let ratay = now.toString();
+  let words = ratay.split(' ');
+  mois.innerHTML = words[1];
+  annee.innerHTML = now.getFullYear();
+  console.log(now);
+  while(jours.hasChildNodes()){  
+    jours.removeChild(jours.firstChild);
+  };
+  for(let i=1; i<getDaysInMonth(now.getMonth()-1, now.getFullYear())+1; i++){
+    let newBtn = document.createElement("button");
+    newBtn.setAttribute("class", "day");
+    let numero = document.createTextNode(i);
+    newBtn.appendChild(numero);
+    jours.appendChild(newBtn);
+  };
+};
+previous.addEventListener("click", precedent);
