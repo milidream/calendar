d66ee3a6759e89068e57efab448941e1d4658577
milidream <bchekir07@gmail.com>
2022-02-23 12:01:22 +0100

    All

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 let month = document.querySelector("#month");
-let jour = document.querySelector(".day");
 let previous = document.querySelector("#previous");
 let next = document.querySelector("#next");
+let jour = document.querySelector(".day");
+
+let add = querySelector("#add");
+let task = querySelector("#task");
+let tasks = querySelector("tasks");
